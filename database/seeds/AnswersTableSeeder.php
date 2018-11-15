fd83594f0c3c7cb50d1467010631196fd53b9b40
on28 <Jr419912@>
2018-11-15 18:35:57 -0500

    profile, answer, question views and navigations

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -11,7 +11,8 @@ class AnswersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = App\User::all();
+        $users = App\User::inRandomOrder();
+        for ($i = 1; $i <= 16; $i++) {
             $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
@@ -19,5 +20,6 @@ class AnswersTableSeeder extends Seeder
                 $answer->question()->associate($question);
                 $answer->save();
             });
+        }
     }
 }
